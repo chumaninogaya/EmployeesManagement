a787100787955a880085df76c37d563e8d8505c7
CNOGAYA <dsk.nogaya@mercedes-benz.com>
2023-08-18 13:53:01 +0200

    Initial commit: Adding new project files

---
+++ src/main/java/com/example/employeesManagementSystem/EmployeesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeesManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeesManagementSystem/EmployeesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeesManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
